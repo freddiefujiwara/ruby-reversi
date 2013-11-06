ba2980b15fc415600c458fdf57b2d3e147d596df
Freddie Fujiwara <github@ze.gs>
2013-11-06 17:24:56 +0900

    direction

--- lib/reversi.rb
+++ lib/reversi.rb
@@ -48,4 +48,72 @@ class Reversi
       return unless @board[y][x] == :blank
       @board[y][x] = color
   end
+
+  def reverse(color,x,y)
+      return unless 1 <= x and x <= @size
+      return unless 1 <= y and y <= @size
+      return unless @board[y][x] == :blank
+      enemy = color == :black ? :white : :black
+      @board[y][x] = color
+      # north
+      (y - 1).downto(1).each do |yi|
+          if @board[yi][x] == color
+              (yi + 1).upto(y - 1).each do |ye|
+                  @board[ye][x] = color
+              end
+              break
+          end
+          break if @board[yi][x] == :blank
+      end
+
+      # south
+      (y + 1).upto(size).each do |yi|
+          if @board[yi][x] == color
+              (yi - 1).downto(y + 1).each do |ye|
+                  @board[ye][x] = color
+              end
+              break
+          end
+          break if @board[yi][x] == :blank
+      end
+
+      # west
+      (x - 1).downto(1).each do |xi|
+          if @board[y][xi] == color
+              (xi + 1).upto(x - 1).each do |xe|
+                  @board[y][xe] = color
+              end
+              break
+          end
+          break if @board[y][xi] == :blank
+      end
+
+      # east
+      (x + 1).upto(size).each do |xi|
+          if @board[y][xi] == color
+              (xi - 1).downto(x + 1).each do |xe|
+                  @board[y][xe] = color
+              end
+              break
+          end
+          break if @board[y][xi] == :blank
+      end
+
+      # south east
+      xi = x
+      yi = y
+      walk = []
+      [y,x].max.upto(size).each do
+        xi += 1
+        yi += 1
+        walk << {:x => xi,:y => yi}
+        if @board[yi][xi] == color
+            walk.each do |pos|
+                @board[pos[:y]][pos[:x]] = color
+            end
+            break
+        end
+        break if @board[yi][xi] == :blank
+      end
+  end
 end
--- spec/reversi_spec.rb
+++ spec/reversi_spec.rb
@@ -47,6 +47,42 @@ describe Reversi do
     end
     it do
         lambda{ @reversi.put :black, 100,100 }.should_not raise_error
+    end
+  end
+  context "Reversi.reverse" do
+    subject { @reversi }
+    it do
+        @reversi.reverse :white, 5,3
+        (3..5).each do |y|
+            @reversi.board[y][5].should == :white
+        end
+
+        @reversi.reverse :black, 5,2
+        @reversi.reverse :black, 5,6
+        (2..6).each do |y|
+            @reversi.board[y][5].should == :black
+        end
+
+        @reversi.reverse :white, 5,1
+        @reversi.reverse :white, 5,7
+        (1..7).each do |y|
+            @reversi.board[y][5].should == :white
+        end
+
+        @reversi.reverse :black, 6,5
+        (4..6).each do |x|
+            @reversi.board[5][x].should == :black
+        end
+
+        @reversi.reverse :black, 6,4
+        @reversi.reverse :black, 3,4
+        (3..6).each do |x|
+            @reversi.board[4][x].should == :black
+        end
+
+        @reversi.reverse :white, 2,3
+        @reversi.board[4][3].should == :white
+        @reversi.board[5][4].should == :white
         puts @reversi
     end
   end
