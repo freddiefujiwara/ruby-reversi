b4135897e1000af8717adcd80def157392c5d8bd
Freddie Fujiwara <github@ze.gs>
2013-11-06 15:13:26 +0900

    wall

--- lib/reversi.rb
+++ lib/reversi.rb
@@ -1,5 +1,18 @@
-require "ruby/reversi/version"
+require "reversi/version"
 
-module Reversi
+class Reversi
+  attr_reader :board
+  attr_reader :size
+  def initialize(size)
+      @size = size
+      @board = Array.new size + 2
+      @board.each_with_index do |arr_y,y|
+          @board[y] = Array.new size + 2
+          @board[y].each_with_index do |arr_x,x|
+            @board[y][x] = :wall if 0 == y or size + 1 == y or 0 == x or size + 1 == x
+          end
+      end
+  end
+
   # Your code goes here...
 end
--- spec/reversi_spec.rb
+++ spec/reversi_spec.rb
@@ -1,11 +1,30 @@
 require 'spec_helper'
 
 describe Reversi do
+  before do
+      @reversi = Reversi.new 8
+  end
   it 'should have a version number' do
     Reversi::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should be_true
+  subject { @reversi }
+  its(:size)  {should == 8}
+  context "Reversi.board" do
+    subject { @reversi.board }
+    it{ should_not be_nil }
+    its(:length) { should == 10}
+    it do
+        [0,9].each do |p|
+            @reversi.board[p].each do |pos|
+                pos.should == :wall
+            end
+        end
+        0.upto(9).each do |y|
+            [0,9].each do |x|
+              @reversi.board[y][x].should == :wall
+            end
+        end
+    end
   end
 end
