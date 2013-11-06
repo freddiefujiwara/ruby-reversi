66d2973a88b2e39e187a33bdb36da78c46c1b4e1
Freddie Fujiwara <github@ze.gs>
2013-11-06 17:43:13 +0900

    north west

--- lib/reversi.rb
+++ lib/reversi.rb
@@ -10,13 +10,13 @@ class Reversi
           @board[y] = Array.new size + 2
           @board[y].each_with_index do |arr,x|
             if 0 == y or size + 1 == y or 0 == x or size + 1 == x
-                @board[y][x] = :wall
+                @board[y][x] = "#"
             elsif (size/2 == y and size/2   == x)   or (size/2+1 == y and size/2+1 == x)
-                @board[y][x] = :white
+                @board[y][x] = "W"
             elsif (size/2 == y and size/2+1 == x)   or (size/2+1 == y and size/2   == x)
-                @board[y][x] = :black
+                @board[y][x] = "B"
             else
-                @board[y][x] = :blank
+                @board[y][x] = " "
             end
           end
       end
@@ -26,16 +26,7 @@ class Reversi
       str = ""
       @board.each_with_index do |arr,y|
           @board[y].each_with_index do |arr,x|
-              case @board[y][x]
-              when :wall
-                  str << "#"
-              when :white
-                  str << "o"
-              when :black
-                  str << "x"
-              when :blank
-                  str << " "
-              end
+              str << @board[y][x]
           end
           str << "\n"
       end
@@ -45,15 +36,15 @@ class Reversi
   def put(color,x,y)
       return unless 1 <= x and x <= @size
       return unless 1 <= y and y <= @size
-      return unless @board[y][x] == :blank
+      return unless @board[y][x] == " "
       @board[y][x] = color
   end
 
   def reverse(color,x,y)
       return unless 1 <= x and x <= @size
       return unless 1 <= y and y <= @size
-      return unless @board[y][x] == :blank
-      enemy = color == :black ? :white : :black
+      return unless @board[y][x] == " "
+      enemy = color == "B" ? "W" : "B"
       @board[y][x] = color
       # north
       (y - 1).downto(1).each do |yi|
@@ -63,7 +54,7 @@ class Reversi
               end
               break
           end
-          break if @board[yi][x] == :blank
+          break if @board[yi][x] == " "
       end
 
       # south
@@ -74,7 +65,7 @@ class Reversi
               end
               break
           end
-          break if @board[yi][x] == :blank
+          break if @board[yi][x] == " "
       end
 
       # west
@@ -85,7 +76,7 @@ class Reversi
               end
               break
           end
-          break if @board[y][xi] == :blank
+          break if @board[y][xi] == " "
       end
 
       # east
@@ -96,7 +87,7 @@ class Reversi
               end
               break
           end
-          break if @board[y][xi] == :blank
+          break if @board[y][xi] == " "
       end
 
       # south east
@@ -113,7 +104,24 @@ class Reversi
             end
             break
         end
-        break if @board[yi][xi] == :blank
+        break if @board[yi][xi] == " "
+      end
+
+      # north west
+      xi = x
+      yi = y
+      walk = []
+      [y,x].min.downto(1).each do
+        xi -= 1
+        yi -= 1
+        walk << {:x => xi,:y => yi}
+        if @board[yi][xi] == color
+            walk.each do |pos|
+                @board[pos[:y]][pos[:x]] = color
+            end
+            break
+        end
+        break if @board[yi][xi] == " "
       end
   end
 end
--- spec/reversi_spec.rb
+++ spec/reversi_spec.rb
@@ -17,18 +17,18 @@ describe Reversi do
     it do
         [0,9].each do |p|
             @reversi.board[p].each do |pos|
-                pos.should == :wall
+                pos.should == "#"
             end
         end
         0.upto(9).each do |y|
             [0,9].each do |x|
-              @reversi.board[y][x].should == :wall
+              @reversi.board[y][x].should == "#"
             end
         end
-        @reversi.board[4][4].should == :white
-        @reversi.board[5][5].should == :white
-        @reversi.board[4][5].should == :black
-        @reversi.board[5][4].should == :black
+        @reversi.board[4][4].should == "W"
+        @reversi.board[5][5].should == "W"
+        @reversi.board[4][5].should == "B"
+        @reversi.board[5][4].should == "B"
     end
   end
   context "Reversi.to_s" do
@@ -38,51 +38,55 @@ describe Reversi do
   context "Reversi.put" do
     subject { @reversi }
     it do
-        @reversi.put :white, 4,6
-        @reversi.board[6][4].should == :white
+        @reversi.put "W", 4,6
+        @reversi.board[6][4].should == "W"
     end
     it do
-        @reversi.put :black, 4,4
-        @reversi.board[4][4].should == :white
+        @reversi.put "B", 4,4
+        @reversi.board[4][4].should == "W"
     end
     it do
-        lambda{ @reversi.put :black, 100,100 }.should_not raise_error
+        lambda{ @reversi.put "B", 100,100 }.should_not raise_error
     end
   end
   context "Reversi.reverse" do
     subject { @reversi }
     it do
-        @reversi.reverse :white, 5,3
+        @reversi.reverse "W", 5,3
         (3..5).each do |y|
-            @reversi.board[y][5].should == :white
+            @reversi.board[y][5].should == "W"
         end
 
-        @reversi.reverse :black, 5,2
-        @reversi.reverse :black, 5,6
+        @reversi.reverse "B", 5,2
+        @reversi.reverse "B", 5,6
         (2..6).each do |y|
-            @reversi.board[y][5].should == :black
+            @reversi.board[y][5].should == "B"
         end
 
-        @reversi.reverse :white, 5,1
-        @reversi.reverse :white, 5,7
+        @reversi.reverse "W", 5,1
+        @reversi.reverse "W", 5,7
         (1..7).each do |y|
-            @reversi.board[y][5].should == :white
+            @reversi.board[y][5].should == "W"
         end
 
-        @reversi.reverse :black, 6,5
+        @reversi.reverse "B", 6,5
         (4..6).each do |x|
-            @reversi.board[5][x].should == :black
+            @reversi.board[5][x].should == "B"
         end
 
-        @reversi.reverse :black, 6,4
-        @reversi.reverse :black, 3,4
+        @reversi.reverse "B", 6,4
+        @reversi.reverse "B", 3,4
         (3..6).each do |x|
-            @reversi.board[4][x].should == :black
+            @reversi.board[4][x].should == "B"
         end
 
-        @reversi.reverse :white, 2,3
-        @reversi.board[4][3].should == :white
-        @reversi.board[5][4].should == :white
+        @reversi.reverse "W", 2,3
+        @reversi.board[4][3].should == "W"
+        @reversi.board[5][4].should == "W"
+
+        @reversi.reverse "W", 7,5
+
+        @reversi.reverse "B", 3,6
         puts @reversi
     end
   end
